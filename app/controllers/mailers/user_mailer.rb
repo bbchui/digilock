5260d83797d57781ead2505c50ffbe0388876a20
Brandon Chui <bbchui325@gmail.com>
2018-01-03 14:52:31 -0800

    add figaro gem

--- app/controllers/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default :from => "bbchui.project@gmail.com"
-
-  def registration_confirmation(user)
-    @user = user
-    mail(:to => "#{user.name} <#{user.username}>", :subject => "Please confirm your registration")
-  end
-
-end
